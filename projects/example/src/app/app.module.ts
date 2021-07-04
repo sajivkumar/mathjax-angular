d1a41e77f0f1f71784aa2347de7bec2a995daf5b
Sajivkumar Velayudha <sajeevkumar@gmail.com>
2021-07-04 15:09:08 +0100

    update - and dependency

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 //
 import { AppComponent } from './app.component';
-import { MathjaxModule } from 'mathjax-lib';
+import { MathjaxModule as DevMathjaxModule } from 'mathjax-lib';
 //
-import { MathjaxModule as MathjaxLiveModule } from 'mathjax-angular';
+import { MathjaxModule } from 'mathjax-angular';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    environment.production
-      ? MathjaxLiveModule.forRoot()
-      : MathjaxModule.forRoot(),
+    // for dev mode testing
+    // DevMathjaxModule.forRoot(),
+    MathjaxModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
